5f07b81a51b20ebe86cdadfce166cc298aa576ea
TheComputerGenie <tim@yourcomputergenie.com>
2019-02-15 20:54:19 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ zcutil/build-mac.sh -j8
 cp src/qt/komodo-qt "$mydir"/PirateWallet
 rm src/qt/komodo-qt
 
-$($pardir/go/bin/appify -name "PirateWallet" -icon "$mydir/src/qt/res/icons/komodo.png" "$mydir/PirateWallet")
-rm "$mydir/PirateWallet"
-open "$mydir/PirateWallet.app"
+#$($pardir/go/bin/appify -name "PirateWallet" -icon "$mydir/src/qt/res/icons/komodo.png" "$mydir/PirateWallet")
+#rm "$mydir/PirateWallet"
+#open "$mydir/PirateWallet.app"
